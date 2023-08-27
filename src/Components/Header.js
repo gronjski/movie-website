fff89a259cb81eb98f3ad7fe993ca556554ede82
gronjski <124671778+gronjski@users.noreply.github.com>
2023-08-27 13:20:21 +0000

    changed  name of website in header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ export function Header ( props ) {
         <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
                 <Navbar.Brand>
-                    Bookworm.com.au
+                    filmster.com.au
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="main-nav" />
                 <Navbar.Collapse id="main-nav">

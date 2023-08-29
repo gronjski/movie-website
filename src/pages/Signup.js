caa6364822362b1b85f5b9ec842bf146277fac4a
gronjski <124671778+gronjski@users.noreply.github.com>
2023-08-29 13:53:19 +0000

    username

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import Button from 'react-bootstrap/Button'
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc } from "firebase/firestore";
 
 import { useState, useEffect, useContext } from "react"
 import { FBAuthContext } from "../contexts/FBAuthContext"
@@ -18,6 +18,7 @@ export function Signup (props) {
     const [ validPassword, setValidPassword ] = useState( false )
     const [ username, setUserName ] = useState("")
     const [ validUserName, setValidUserName ] = useState( false )
+    const [userNameFeedback, setUserNameFeedback] = useState()
 
     const FBAuth = useContext(FBAuthContext)
     const FBDb = useContext(FBDbContext)
@@ -32,17 +33,21 @@ export function Signup (props) {
         const docSnap = await getDoc(ref)
         if (docSnap.exists()) {
             //user already exists
-            console.log("exists")
+            // console.log("exists")
+            setUserNameFeedback("username is already taken")
+            setValidUserName(false)
         }
         else {
-            //user doesn't exist
-            console.log("doesn't exist")
+            // user doesn't exist
+            // console.log("doesn't exist")
+            setUserNameFeedback(null)
+            setValidUserName(true)
         }
     }
 
     useEffect(() => {
         let userLength = false
-        let noIllegalChars = false
+        let IllegalChars = []
         // check if the username is of a certain length
         if (username.length < 5) {
             userLength = false
@@ -54,14 +59,11 @@ export function Signup (props) {
       const chars = Array.from(username)
       chars.forEach((chr) => {
         if (allowedChars.includes(chr) === false) {
-          noIllegalChars = false
-        }
-        else {
-          noIllegalChars = true
+          IllegalChars.push (chr)
         }
       })
       // check if username does not exist in Firebase if the other two checks are true
-      if (userLength === true && noIllegalChars === true) {
+      if (userLength === true && IllegalChars.length === 0) {
         clearTimeout(timer)
         timer = setTimeout(() => { checkUser(username) }, 1500)
       }     
@@ -85,12 +87,19 @@ export function Signup (props) {
         }
     }, [password])
 
+    const AddUserName = async () => {
+        await setDoc(doc(FBDb, "usernames", username ), {
+          name: username
+        })
+      }
+
     const SignUpHandler = () => {
         createUserWithEmailAndPassword( FBAuth, email, password )
         .then ( ( user ) => {
             // user is created in Firebase
             // console.log (user)
             // alert user that account has been created
+            AddUserName()
             navigate ("/")
         })
         .catch( (error) => {
@@ -115,7 +124,10 @@ export function Signup (props) {
                                 placeholder="unique username"
                                 onChange={(evt) => setUserName(evt.target.value)}
                                 value={username}
+                                isValid={validUserName}
                             />
+                            <Form.Control.Feedback>Looks good</Form.Control.Feedback>
+                            <Form.Control.Feedback type="invalid">{userNameFeedback}</Form.Control.Feedback>
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Email address</Form.Label>
@@ -140,7 +152,7 @@ export function Signup (props) {
                             type="submit" 
                             className="my-2 w-100" 
                             size="lg" 
-                            disabled = { ( validEmail && validPassword ) ? false: true }
+                            disabled = {(validEmail && validPassword && validUserName) ? false: true}
                         >
                             Sign up
                         </Button>

4ab000d43d4a8d2fd67d90cafa67c66fdc99d312
gronjski <124671778+gronjski@users.noreply.github.com>
2023-09-03 11:54:53 +0000

    Added About and Contact text and style

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,22 @@
+import '../styles/About.css'
+
 export function About () {
     return (
-        <div>
-            <h1>About</h1>
+        <div className='body'>
+            <h1>About Filmster</h1>
+            <p>Welcome to Filmster, your ultimate destination for everything related to movies and cinematic experiences. At Filmster, we are passionate about the art of storytelling through film and are dedicated to providing you with insightful movie reviews, in-depth analysis, and a community of film enthusiasts who share your love for the silver screen.</p>
+            <h2>Our Mission</h2>
+            <p>Filmster was born out of a shared love for movies and a desire to create a space where film aficionados and casual moviegoers alike can come together to explore the world of cinema. Our mission is simple: to provide you with the most comprehensive and honest movie reviews, ensuring that you make informed decisions about what to watch next.</p>
+            <h2>Why Choose Filmster</h2>
+            <p>Trustworthy Reviews: We take our role as movie reviewers seriously, ensuring our opinions are unbiased and our critiques are fair. You can rely on us to provide honest assessments that help you decide which movies are worth your time.
+
+                Diverse Perspectives: Our team consists of individuals from various backgrounds and tastes in film, offering a range of perspectives to cater to all movie preferences.
+
+                Passion for Film: Filmster is not just a website; it's a labor of love. We are passionate about films, and our dedication shines through in every review and article we publish.
+
+                User-Friendly Experience: Our website is designed with you in mind. It's easy to navigate, making it simple to find the information you need and connect with other film enthusiasts.</p>
+
+            <p>Thank you for visiting Filmster, your go-to destination for movie reviews and everything cinema-related. Whether you're a die-hard film buff or just looking for your next movie night pick, Filmster is here to enhance your cinematic journey. Join us in celebrating the magic of movies, one review at a time.</p>
         </div>
     )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,18 @@
 export function Contact () {
     return (
-        <div>
-            <h1>Contact</h1>
+        <div className="body">
+            <h1>Contact Us</h1>
+            <p>Thank you for your interest in reaching out to Filmster. We value your feedback, questions, and suggestions, and we're here to assist you in any way we can. Below are the various methods you can use to get in touch with us.</p>
+            <h2>General Inquiries</h2>
+            <p>If you have general questions or feedback about Filmster, please feel free to reach out to us at <b>info@filmster.com</b></p>
+            <h2>Connect with us on Socials</h2>
+            <ul>
+                <li><b>Facebook</b>: FilsmterOfficial</li>
+                <li><b>X</b>: @FilmsterX</li>
+                <li><b>Insta</b>: @fimlstergram</li>
+                <li><b>YouTuve</b>: Filmster Channel</li>
+            </ul>
+            <p>We strive to respond to all inquiries as promptly as possible. Thank you for your interest in Filmster, and we look forward to hearing from you. Your feedback and engagement are essential in helping us continue to provide quality content and a vibrant community for movie enthusiasts.</p>
         </div>
     )
 }
